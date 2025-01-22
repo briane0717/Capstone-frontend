2a942fa40bfd86824c7fafaac6c4d85f9992bb8e
Brian <briane0717@gmail.com>
2025-01-21 19:54:39 -0500

    added form with shipping info inputs

--- capstone-frontend/src/pages/CheckoutPage.jsx
+++ capstone-frontend/src/pages/CheckoutPage.jsx
@@ -7,11 +7,97 @@ const CheckoutPage = () => {
       street: "",
       city: "",
       state: "",
-      postleCode: "",
+      postalCode: "",
       country: "",
     },
   });
-  return <div></div>;
+  const [error, setError] = useState(null);
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>Shipping Information</h2>
+
+        <label>
+          <input
+            type="text"
+            name="firstName"
+            placeholder="First Name"
+            value={shippingInfo.firstName}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <input
+            type="text"
+            name="lastName"
+            placeholder="Last Name"
+            value={shippingInfo.lastName}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <h3>Shipping Address</h3>
+
+        <label>
+          <input
+            type="text"
+            name="street"
+            placeholder="Address"
+            value={shippingInfo.street}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <input
+            type="text"
+            name="city"
+            placeholder="City"
+            value={shippingInfo.city}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <input
+            type="text"
+            name="state"
+            placeholder="State"
+            value={shippingInfo.state}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <input
+            type="text"
+            name="postalCode"
+            placeholder="Postal Code"
+            value={shippingInfo.postalCode}
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <label>
+          <input
+            type="text"
+            name="country"
+            placeholder="Country"
+            value={shippingInfo.country}
+            onChange={handleChange}
+            required
+          />
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default CheckoutPage;
